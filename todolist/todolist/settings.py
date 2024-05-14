b5dc7c32bc318e777294f5fc4643cea1c6a9a066
Wera Barinova <alisawera@gmail.com>
2024-05-14 22:06:41 +0300

    Настроили БД

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "tudushka"
+        }
     }
 }
+
 
 
 # Password validation
