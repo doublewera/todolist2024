6e77eecaba40c3efc18a519655fa96d9b9958b12
Wera Barinova <alisawera@gmail.com>
2024-05-28 20:31:24 +0300

    Добавлена переменная в шаблон

--- todolist/tasklist/views.py
+++ todolist/tasklist/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context = {}
+    tasks = [
+        'Рассказать урок',
+        'Дать задание',
+        'Проверить задани',
+        'Объяснить домашку',
+    ]
+    context = {
+        'kogda': 'сегодня',
+    }  
     return render(
         request,                # Запрос
 	    'tasklist/index.html',  # путь к шаблону
-        context                 # подстановки
+        context               # подстановки
     )
 
