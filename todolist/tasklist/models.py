faf8a43d8222f32a5ba719a8829a1e8edfdd3861
Wera Barinova <alisawera@gmail.com>
2024-05-31 20:37:48 +0300

    Создали функицю рендеринга формы в шаблон
    Создали запрос на запись в БД (form.save())

--- todolist/tasklist/models.py
+++ todolist/tasklist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from colorfield.fields import ColorField
 from datetime import datetime
 
 # Create your models here.
@@ -8,3 +9,4 @@ class Task(models.Model):
     deadline = models.DateTimeField(null=True)
     description = models.CharField(max_length=512)
     done = models.BooleanField(default=False)
+    category = ColorField(default=[255, 0, 0])
--- todolist/tasklist/views.py
+++ todolist/tasklist/views.py
@@ -18,3 +18,20 @@ def index(request):
         context                 # подстановки
     )
 
+from .forms import TaskForm
+
+def new_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+    #else:
+    context = {
+        'form': TaskForm(),
+    }
+    return render(
+        request,                  # Запрос
+	    'tasklist/newtask.html',  # путь к шаблону
+        context                   # подстановки
+    )
