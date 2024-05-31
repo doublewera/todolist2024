667f6c8caeb1b78b7b4902380a180cf03b18bc94
Wera Barinova <alisawera@gmail.com>
2024-05-31 20:38:54 +0300

    Прописали новый путь для отображения формы и
    ОБРАБОТКИ ПОСТ-ЗАПРОСА из ФОРМЫ!

--- todolist/tasklist/urls.py
+++ todolist/tasklist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, new_task
 
 urlpatterns = [
     path('', index),
+    path('newtask/', new_task, name='new_task'),
 ]
