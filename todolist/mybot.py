3d9a8ef0e31a468e017c1c9a3a73d913a3addd57
Wera Barinova <alisawera@gmail.com>
2024-08-20 20:40:50 +0300

    Теперь наш бот умеет работать с Postgres через Django

--- todolist/mybot.py
+++ todolist/mybot.py
@@ -1,4 +1,9 @@
 from todolist.secret import token
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist.settings')
+import django
+django.setup()
+from tasklist.models import Task
 import telebot
 
 class MyBot(telebot.TeleBot):
@@ -29,6 +34,7 @@ class MyBot(telebot.TeleBot):
         f.close()
 
 
+
 bot = MyBot(token)
 
 @bot.message_handler(commands= ['start'])
@@ -39,4 +45,20 @@ def start(message):
         message.chat.id,
         '<b>Пользователю: готов!</b>', parse_mode='html')
     
+@bot.message_handler(commands= ['list'])
+def start(message):
+    tasks = Task.objects.all()
+    print(tasks)
+    print(message.text)
+    new_message = ""
+    for t in tasks:
+        new_message += "%s %s %s %s\n" % (
+            t.given, t.deadline, t.done, t.description
+        )
+    bot.add_user(message)
+    bot.send_message(
+        message.chat.id,
+        '<b>Твои задачи: </b>\n%s' % new_message,
+        parse_mode='html')
+
 bot.polling(none_stop=True)
