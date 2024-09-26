adf59fcd824bba96c3820cb116802d900ab72bdf
Wera Barinova <alisawera@gmail.com>
2024-09-26 20:00:49 +0300

    Добавлены изменения в модель: появилось необязательное поле, ссылающееся на пользователя Выполнены миграции

--- todolist/tasklist/models.py
+++ todolist/tasklist/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Task(models.Model):
+    customer = models.ForeignKey(User, related_name='customer', on_delete=models.CASCADE, null=True, blank=True, default=None)
+    executor = models.ForeignKey(User, related_name='executor', on_delete=models.CASCADE, null=True, blank=True, default=None)
     given = models.DateTimeField(default=datetime.now)
+    softline = models.DateTimeField(null=True, blank=True, default=None)
     deadline = models.DateTimeField(null=True)
     description = models.CharField(max_length=512)
     done = models.BooleanField(default=False)
