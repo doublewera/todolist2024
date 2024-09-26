0f8ca50bf89f7c30ef43477d9136ea6872b9b4fa
Wera Barinova <alisawera@gmail.com>
2024-09-26 22:01:29 +0300

    Добавлен календарь
    Добавлен шаблон с листалкой календарных недель по датам
    Добавлены пути для сервера, чтобы он искал функцию для рендеринга календаря в  шаблон
    Добавлена функция во views, получающая задачи из базы и рендерящая их в шаблон
    Также есть функция, формирующая удобный тип данных для рендерига

--- todolist/tasklist/urls.py
+++ todolist/tasklist/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path
-from .views import index, new_task, get_all_tasks
+from django.urls import path, re_path
+from . import views
 
 urlpatterns = [
     # tasklist/  Приехало из глобального юрлз
-    path('', index, name='tasklist'),
+    path('',                  views.index,         name='tasklist'),
     
     # tasklist/newtask/
-    path('newtask/', new_task, name='new_task'),
+    path('newtask/',          views.new_task,      name='new_task'),
     
     # tasklist/update_tasks/
-    path('update_tasks/', get_all_tasks, name='update')
+    path('update_tasks/',     views.get_all_tasks, name='update'),
+
+    # Calendar view
+    path('calendar/',         views.calendar,      name='calendar_now'),
+
+    # Calendar view
+    path('calendar/<str:dtstr>/', views.calendar,      name='calendar'),
 ]
--- todolist/tasklist/views.py
+++ todolist/tasklist/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import Task
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 
 def task_desc(tasks):
@@ -56,4 +56,81 @@ def new_task(request):
         request,                  # Запрос
 	    'tasklist/newtask.html',  # путь к шаблону
         context                   # подстановки
+    )
+
+# timeslots = {
+#    '10:00': {
+#            'time': '10:00',
+#            'weekdays': [
+#                {'free': True},  # 0
+#                {'free': True},  # 0
+#                {'free': True},  # 0
+#                {'free': True},  # 0
+#                {'free': True},  # 0
+#                {'free': True},  # 0
+#                {'free': True},  # 0
+#            ]
+#        }
+# }
+def newWeek(dt):
+    work_starts = 10   # 10:00 утра
+    slots_per_day = 23  # 12 по полчаса
+    # Создадим слоты
+    timeslots = {}
+    s = datetime(dt.year, dt.month, dt.day, work_starts)
+    while s < dt + timedelta(7):  # next week
+        wd = []
+        for i in range(7):
+            wd.append({
+                'free': True
+            })
+        tm = s.strftime('%H:%M')
+        timeslots[tm] = {
+            'time': tm,
+            'weekdays': wd
+        }
+        s += timedelta(seconds=1800)  # 3600 = 60*60 - это час в секундах
+        if s.hour > work_starts + slots_per_day / 2:
+            s += timedelta(1)
+            s = datetime(s.year, s.month, s.day, work_starts)
+    return timeslots
+
+def calendar(request, dtstr=''):
+    dt = datetime.now()
+    try:
+        dt = datetime.strptime(dtstr, '%Y%m%d')
+    except:
+        return redirect(dt.strftime('/tasklist/calendar/%Y%m%d'))
+    if dt.weekday():  # 0 - понедельник
+        return redirect((dt - timedelta(dt.weekday())).strftime('/tasklist/calendar/%Y%m%d'))
+    dt_next = dt + timedelta(7)  # next week
+    timeslots = newWeek(dt)
+
+    # Все задачи на эту неделю
+    tasks = Task.objects.filter(  # Достаточно фильтровать по одному параметру, если все слоты умещаются в текущие сутки и никто заполночь не работает
+        deadline__gte=dt.strftime('%Y-%m-%d')
+    ).filter(deadline__lte=dt_next.strftime('%Y-%m-%d'))
+    print(tasks)
+    for t in tasks:
+        tm = t.softline.strftime('%H:%M')
+        #print(t.description, t.softline, tm)
+        #print(timeslots[tm])
+        if tm in timeslots:
+            s = timeslots[tm]['weekdays'][t.softline.weekday()]
+            s['free'] = False
+            s['task'] = t
+    # Превращаем в список, чтобы потом не мучиться упорядочиваением
+    timeslots_list = []
+    for key in sorted(list(timeslots.keys())):
+        timeslots_list.append(timeslots[key])
+    context = {
+        'prev_week': (dt - timedelta(7)).strftime('%Y%m%d'),
+        'curr_week': (dt).strftime('%Y%m%d'),
+        'next_week': (dt + timedelta(7)).strftime('%Y%m%d'),
+        'timeslots': timeslots_list
+    }
+    return render(
+        request,                    # Запрос
+	    'tasklist/calendar.html',   # путь к шаблону
+        context                     # подстановки
     )
