5e017ad10605502e7f0d37d70627545510650006
vegner_a@ru.itstep.org <vegner_a@t.top-academy.ru>
2024-10-10 19:59:38 +0300

    Не проверено: так можно активировать базу позже, а не при старте сервера

--- todolist/mainpage/apps.py
+++ todolist/mainpage/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
-
 class MainpageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainpage'
+
+#from todolist.settings import DATABASES
+#
+#class YourAppConfig(AppConfig):
+#
+#    def ready(self):
+#        DATABASES.update({})
