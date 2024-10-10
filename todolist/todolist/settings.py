67314a54755e70a2ae260a366a68298383491121
vegner_a@ru.itstep.org <vegner_a@t.top-academy.ru>
2024-10-10 19:56:03 +0300

    Секретный ключ может создаваться каждый раз заново при старте сервера

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -19,7 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-from .secret import SECRET_KEY
+#from .secret import SECRET_KEY
+from django.core.management.utils import get_random_secret_key  
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
